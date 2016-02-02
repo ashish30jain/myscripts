4040dadb88510a8b5ab26cb27ac590f404c5c045
ashish30jain <jashishtech@gmail.com>
2016-02-02 15:37:22 +0530

    Program to find a number which generate largest chain under 1 million.
    asda

---
+++ collatz-script.py
@@ -0,0 +1,26 @@
+#Script to generate longest chain of odd even numbers.
+
+collatz_counts = {}
+
+def collatz(n):
+    count = 1
+    start = n
+    while n!=1:
+        if n in collatz_counts:
+            count += collatz_counts[n]
+            break
+        if n%2==0: n/=2
+        else: n = 3*n+1
+        count += 1
+
+    collatz_counts[start] = count
+    return count
+
+answer = max(((collatz(i),i) for i in range(1,1000000)))
+print("The starting position generating the longest sequence is",answer[1])
+
+def main():
+  collatz(1)
+
+if __name__ == "__main__":
+  main()
